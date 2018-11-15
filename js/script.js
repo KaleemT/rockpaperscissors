42af48ae6ac7f61c38ea3360305dc1e5cb8f0def
KaleemT <kaleemt02@gmail.com>
2018-11-15 23:37:46 +0000

    project

--- js/script.js
+++ js/script.js
@@ -4,12 +4,21 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
+var userChoice="";
+var computerChoice="";
+var winner="";
+var randomNumber=0;
 
 // DOCUMENT READY FUNCTION BELOW
 
 $("#shoot").click(function() {
-$("#userChoice").text($("#input").val());
+    
+    userChoice=$("#input").val()
+    
+    $("#userChoice").text(userChoice);
+    
+    var randomNumber= Math.random();
+    
+    
 });
-
+ 
